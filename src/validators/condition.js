1de49c563357394bf7dbfb81e0a3b7a290e186d7
Maslow <wangfugen@126.com>
2019-09-03 18:22:15 +0800

    remove useless code & logs

--- src/validators/condition.js
+++ src/validators/condition.js
@@ -1,19 +1,20 @@
 const vm = require('vm')
 
-function ConditionHandler(config, {ruler, collection, action, query, data, options, injections}){
-    try {
-        let script = this._script
-        if(!script) {
-            script = new vm.Script(config)
-            this._script = script
-        }else{
-            console.log({obj: this})
-        }
-        const context = {...injections, query, data, options}
-        return script.runInNewContext(context)
-    } catch (error) {
-        return false
+function ConditionHandler (
+  config,
+  { ruler, collection, action, query, data, options, injections }
+) {
+  try {
+    let script = this._script
+    if (!script) {
+      script = new vm.Script(config)
+      this._script = script
     }
+    const context = { ...injections, query, data, options }
+    return script.runInNewContext(context)
+  } catch (error) {
+    return false
+  }
 }
 
 module.exports = ConditionHandler
--- tests/units/entry_test.js
+++ tests/units/entry_test.js
@@ -1,7 +1,7 @@
 const assert = require('assert')
 const { Entry, Ruler, Accessor } = require('../../src/index')
 
-describe('class Entry', async () => {
+describe('class Entry', () => {
   it('constructor() ok', () => {
     const entry = new Entry()
     assert.ok(entry._ruler instanceof Ruler)

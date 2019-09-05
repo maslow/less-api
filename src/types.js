84e43b7a503ba0885b538ac7fa4a4026efc1e8f3
Maslow <wangfugen@126.com>
2019-09-06 01:23:45 +0800

    format codes

--- src/types.js
+++ src/types.js
@@ -1,29 +1,28 @@
-
 const actions = {
-    READ: 'database.queryDocument',
-    UPDATE: 'database.updateDocument',
-    ADD: 'database.addDocument',
-    REMOVE: 'database.deleteDocument'
+  READ: 'database.queryDocument',
+  UPDATE: 'database.updateDocument',
+  ADD: 'database.addDocument',
+  REMOVE: 'database.deleteDocument'
 }
 
 const permissions = {
-    READ: '.read',
-    UPDATE: '.update',
-    ADD: '.add',
-    REMOVE: '.remove'
+  READ: '.read',
+  UPDATE: '.update',
+  ADD: '.add',
+  REMOVE: '.remove'
 }
 
 const actionMap = {
-    [actions.READ]: '.read',
-    [actions.UPDATE]: '.update',
-    [actions.ADD]: '.add',
-    [actions.REMOVE]: '.remove'
+  [actions.READ]: '.read',
+  [actions.UPDATE]: '.update',
+  [actions.ADD]: '.add',
+  [actions.REMOVE]: '.remove'
 }
 const acceptParams = {
-    [actions.READ]: ['query', 'order', 'offset', 'limit', 'projection', 'multi'],
-    [actions.UPDATE]: ['query', 'data', 'multi', 'upsert', 'merge'],
-    [actions.ADD]: ['data', 'multi'],
-    [actions.REMOVE]: ['query', 'multi']
+  [actions.READ]: ['query', 'order', 'offset', 'limit', 'projection', 'multi'],
+  [actions.UPDATE]: ['query', 'data', 'multi', 'upsert', 'merge'],
+  [actions.ADD]: ['data', 'multi'],
+  [actions.REMOVE]: ['query', 'multi']
 }
 
 const UPDATE_COMMANDS = {
@@ -34,36 +33,36 @@ const UPDATE_COMMANDS = {
   PUSH: '$push',
   POP: '$pop',
   SHIFT: '$pop',
-  UNSHIFT: '$push',
+  UNSHIFT: '$push'
 }
 
 const LOGIC_COMMANDS = {
-    AND: '$and',
-    OR: '$or',
-    NOT: '$not',
-    NOR: '$nor',
+  AND: '$and',
+  OR: '$or',
+  NOT: '$not',
+  NOR: '$nor'
 }
 
 const QUERY_COMMANDS = {
-    EQ: '$eq',
-    NEQ: '$ne',
-    GT: '$gt',
-    GTE: '$gte',
-    LT: '$lt',
-    LTE: '$lte',
-    IN: '$in',
-    NIN: '$nin',
-    GEO_NEAR: '$geoNear',
-    GEO_WITHIN: '$geoWithin',
-    GEO_INTERSECTS: '$geoIntersects'
-  }
+  EQ: '$eq',
+  NEQ: '$ne',
+  GT: '$gt',
+  GTE: '$gte',
+  LT: '$lt',
+  LTE: '$lte',
+  IN: '$in',
+  NIN: '$nin',
+  GEO_NEAR: '$geoNear',
+  GEO_WITHIN: '$geoWithin',
+  GEO_INTERSECTS: '$geoIntersects'
+}
 
 module.exports = {
-    actions,
-    permissions,
-    actionMap,
-    acceptParams,
-    UPDATE_COMMANDS,
-    LOGIC_COMMANDS,
-    QUERY_COMMANDS
+  actions,
+  permissions,
+  actionMap,
+  acceptParams,
+  UPDATE_COMMANDS,
+  LOGIC_COMMANDS,
+  QUERY_COMMANDS
 }

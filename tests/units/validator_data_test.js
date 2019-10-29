7b161508886af10da863b6f1da13bd200a06d42f
Maslow <wangfugen@126.com>
2019-10-29 09:41:22 +0800

    add tests for data validator

---
+++ tests/units/validator_data_test.js
@@ -0,0 +1,388 @@
+const assert = require('assert')
+const {  Ruler } = require('../../src/index')
+
+
+
+describe('Data Validator - required', () => {
+    const rules = {
+        categories: {
+            ".update": {
+                condition: true,
+                data: { 
+                    title: { required: true },
+                    content: {},
+                    author: { required: false }
+                }
+            }
+        }
+    }
+
+    const ruler = new Ruler()
+    ruler.load(rules)
+
+    let params = {
+        collection: 'categories', action: 'database.updateDocument'
+    }
+
+
+    it('data == undefined should be rejected', async () => {
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'data is undefined')
+    })
+
+    it('data is not object should be rejected', async () => {
+        params.data = "invalid type"
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'data must be an object')
+    })
+
+    it('required == true should be ok', async () => {
+        params.data = {
+            title: 'Title'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('empty data should be rejected', async () => {
+        params.data = {
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'title is required')
+    })
+
+    it('required == true should be rejected', async () => {
+        params.data = {
+            content: 'Content'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'title is required')
+    })
+
+    it('required == false should be ok', async () => {
+        params.data = {
+            title: 'Title',
+            content: 'Content',
+            author: 'Author'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+})
+
+describe('Data Validator - length', () => {
+    const rules = {
+        categories: {
+            ".update": {
+                condition: true,
+                data: { 
+                    title: { length: [3, 6], required: true},
+                    content: { length: [3, 6]}
+                }
+            }
+        }
+    }
+
+    const ruler = new Ruler()
+    ruler.load(rules)
+
+    let params = {
+        collection: 'categories', action: 'database.updateDocument'
+    }
+
+
+    it('length == min should be ok', async () => {
+        params.data = {
+            title: 'abc'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('length == max should be ok', async () => {
+        params.data = {
+            title: '123456'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('length < min should be rejected', async () => {
+        params.data = {
+            title: 'ab'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'length of title should >= 3 and <= 6')
+    })
+    
+
+    it('length > max should be rejected', async () => {
+        params.data = {
+            title: '1234567'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'length of title should >= 3 and <= 6')
+    })
+
+    it('length < min && require == false should be rejected', async () => {
+        params.data = {
+            title: 'good',
+            content: 'a'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'length of content should >= 3 and <= 6')
+    })
+})
+
+describe('Data Validator - default', () => {
+    const rules = {
+        categories: {
+            ".update": {
+                condition: true,
+                data: { 
+                    title: { default: 'Default Title', required: true},
+                    content: { default: 0 }
+                }
+            }
+        }
+    }
+
+    const ruler = new Ruler()
+    ruler.load(rules)
+
+    let params = {
+        collection: 'categories', action: 'database.updateDocument'
+    }
+
+
+    it('default should be applied both required equals to true and false', async () => {
+        params.data = {
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+
+        assert.equal(params.data.title, 'Default Title')
+        assert.equal(params.data.content, 0)
+    })
+
+    it('given value should replace default value', async () => {
+        params.data = {
+            title: 'Custom Title'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+
+        assert.equal(params.data.title, 'Custom Title')
+        assert.equal(params.data.content, 0)
+    })
+
+    it('given value should replace default value both required == true and false', async () => {
+        params.data = {
+            title: 'Custom Title',
+            content: 'Custom Content'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+
+        assert.equal(params.data.title, 'Custom Title')
+        assert.equal(params.data.content, 'Custom Content')
+    })
+})
+
+describe('Data Validator - in', () => {
+    const rules = {
+        categories: {
+            ".update": {
+                condition: true,
+                data: { 
+                    title: { in: [true, false]},
+                    content: { in: ['China', 'Russia'] }
+                }
+            }
+        }
+    }
+
+    const ruler = new Ruler()
+    ruler.load(rules)
+
+    let params = {
+        collection: 'categories', action: 'database.updateDocument'
+    }
+
+
+    it('empty data should be ok', async () => {
+        params.data = {
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('valid data should be ok ', async () => {
+        params.data = {
+            title: false,
+            content: 'China'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('invalid data should return an error ', async () => {
+        params.data = {
+            content: 'invalid value'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'invalid content')
+    })
+
+    it('invalid data for boolean value should return an error ', async () => {
+        params.data = {
+            title: 1,
+            content: 'China'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'invalid title')
+    })
+})
+
+
+describe('Data Validator - number', () => {
+    const rules = {
+        categories: {
+            ".update": {
+                condition: true,
+                data: { 
+                    total: { number: [0, 100] },
+                }
+            }
+        }
+    }
+
+    const ruler = new Ruler()
+    ruler.load(rules)
+
+    let params = {
+        collection: 'categories', action: 'database.updateDocument'
+    }
+
+
+    it('number == min should be ok', async () => {
+        params.data = {
+            total: 0
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('number == max should be ok', async () => {
+        params.data = {
+            total: 100
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('number < min should be rejected', async () => {
+        params.data = {
+            total: -1
+        }
+
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'total should >= 0 and <= 100')
+    })
+    
+
+    it('number > max should be rejected', async () => {
+        params.data = {
+            total: 101
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(!r)
+        assert.ok(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'total should >= 0 and <= 100')
+    })
+})
+
+
+describe('Data Validator - match', () => {
+    const rules = {
+        categories: {
+            ".update": {
+                condition: true,
+                data: { 
+                    account: { match: "^\\d{6,10}$" },
+                }
+            }
+        }
+    }
+
+    const ruler = new Ruler()
+    ruler.load(rules)
+
+    let params = {
+        collection: 'categories', action: 'database.updateDocument'
+    }
+
+
+    it('match should be ok', async () => {
+        params.data = {
+            account: '1234567'
+        }
+        const [err, r] = await ruler.validate(params)
+        assert.ok(r)
+        assert.ok(!err)
+    })
+
+    it('match invalid value should return an error', async () => {
+        params.data = {
+            account: 'abc'
+        }
+        const [err, r] = await ruler.validate(params)
+
+        assert.ok(!r)
+        assert.equal(err.length, 1)
+        assert.equal(err[0].type, 'data')
+        assert.equal(err[0].error, 'account had invalid format')
+    })
+})
